0101f1efead10c2dd07793c0cfc5c8cc3080294a
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-11 17:04:59 +0000

    Automatically backed up by Learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,13 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+<<<<<<< HEAD
     provider :developer unless Rails.env.production?
     # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 
     provider :github, ENV['e23efbfdf124c209ed5e'], ENV['2c7eeb735bd6294fb4a1645af01cd00bddfcc470']
   end
 
+=======
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  end 
+>>>>>>> 2fc0ad351d47b7e597b848680473ef6d17b1f12f
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
 
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   root 'welcome#home'
+=======
+  # Add your routes here
+  root 'welcome#home'
+
+
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+end
+>>>>>>> 2fc0ad351d47b7e597b848680473ef6d17b1f12f
 end
